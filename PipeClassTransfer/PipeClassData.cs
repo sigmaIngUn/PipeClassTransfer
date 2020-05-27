9dd0a42a9d7034c7ecd67574b714bf995c16f10a
Schneider <schneider@rohr2.de>
2020-05-27 10:47:45 +0200

    ADDED LICENSE-TEXTS AND HEADERS
    Added License-Text for LGP and GPL, added File Headers for
    Copyright and License information

--- PipeClassTransfer/PipeClassData.cs
+++ PipeClassTransfer/PipeClassData.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections.Generic;
+﻿// This file PipeClassData.cs is part of PipeClassTransfer
+// Copyright (C) 2020 SIGMA Ingenieurgesellschaft mbH, Unna, Germany
+// 
+// PipeClassTransfer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// PipeClassTransfer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with PipeClassTransfer.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
 
 namespace PipeClassTransfer
 {
