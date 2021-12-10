6a816d9d66373be67b00c9b921807c479ed0ca34
oelschlaeger <oelschlaeger@rohr2.de>
2021-12-10 16:00:02 +0100

    Added class Flange

--- PipeClassTransfer/Component.cs
+++ PipeClassTransfer/Component.cs
@@ -26,6 +26,7 @@ namespace PipeClassTransfer
     [System.Xml.Serialization.XmlInclude(typeof(Reducer))]
     [System.Xml.Serialization.XmlInclude(typeof(Cap))]
     [System.Xml.Serialization.XmlInclude(typeof(Blank))]
+    [System.Xml.Serialization.XmlInclude(typeof(Flange))]
     public class Component
     {
         /// <summary>
